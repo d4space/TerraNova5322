b637f2e7a06de1c28ee7461645ff481fcf9725ef
jungmin <jungmin@cern.ch>
2014-01-20 18:42:21 +0900

    unnecessary file remove

--- WAnalyzer/analysis/EventSelection/include/2012HighPU/chain_Electron_TTJets_S10.h
+++
@@ -1,6 +0,0 @@
-void chain_Electron_TTJets_S10( TChain *chains)
-{
-  chains->Add("/d1/scratch/jungmin/cms/CMSSW_5_3_9/src/knuphy_back/WAnalyzer/test/McS10_2012/batch/TTJets_MassiveBinDECAY//wNTuple1.root");
-  chains->Add("/d1/scratch/jungmin/cms/CMSSW_5_3_9/src/knuphy_back/WAnalyzer/test/McS10_2012/batch/TTJets_MassiveBinDECAY//wNTuple2.root");
-  chains->Add("/d1/scratch/jungmin/cms/CMSSW_5_3_9/src/knuphy_back/WAnalyzer/test/McS10_2012/batch/TTJets_MassiveBinDECAY//wNTuple3.root");
-}
