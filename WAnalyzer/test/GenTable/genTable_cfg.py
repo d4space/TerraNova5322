34132099c932ef595ef09722823f6fae78216114
SangEun Lee <d4space@gmail.com>
2014-02-18 22:19:56 +0100

    haha

---
+++ WAnalyzer/test/GenTable/genTable_cfg.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("wNTuple")
+process.load("KoSMP.WAnalyzer.wAnalysis_MC_cff")
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger = cms.Service("MessageLogger")
+#process.MessageLogger.destinations = ['cout']
+#process.MessageLogger.cout = cms.untracked.PSet(
+#    threshold = cms.untracked.string('INFO'),
+#    FwkReport = cms.untracked.PSet(reportEvery=cms.untracked.int32(1000))
+#)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring()
+)
+
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.printTree = cms.EDAnalyzer("ParticleListDrawer",
+    maxEventsToPrint = cms.untracked.int32(100),
+    printVertex = cms.untracked.bool(False),
+    src = cms.InputTag("genParticles")
+    )
+process.load("KoSMP.WAnalyzer.Sources.WplusToMuNu_S8_8TeV_AODSIM_PatSkim_local_cff")
+
+process.p = cms.Path(
+    process.printTree
+)
+
