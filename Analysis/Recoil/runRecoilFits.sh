6cf98ef640a102d02d4115dbf5edfe1ce8634e13
Khakim <khakimjan@yahoo.com>
2014-05-27 01:25:08 +0200

    hahaha

--- Analysis/Recoil/runRecoilFits.sh
+++ Analysis/Recoil/runRecoilFits.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
 #Muon
-#INPUTDIR=/terranova_0/W/CMSSW_5_2_6/src/TerraNova/NtupleMaker/analysis/MuNeu/W_Pt/results
-INPUTDIR=/u/user/sangilpark/WorkDir/Muon/TerraNova_Nominal/NtupleMaker/analysis/EventSelection/MuonLowPU
+INPUTDIR=../WPtRecoilCorr/Muon2012LoPU
 ###                           u1 model(1:single,2:double,3:triple), u2 model, signal only
-root -l -q fitRecoil.C+\(\"${INPUTDIR}/RDRecoil.root\",2,2,1,\"ZmmData\",\"Z\"\)
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/Muon_RD_LowPU_RecoilEvaRD.root\",2,2,1,\"ZmmData\",\"Z\"\)
 rm *.so *.d
-root -l -q fitRecoil.C+\(\"${INPUTDIR}/DYToMuMuRecoil.root\",2,2,1,\"ZmmMC\",\"Z\"\)
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/Muon_DYToMuMu_S8_RecoilEvaMC.root\",2,2,1,\"ZmmMC\",\"Z\"\)
 rm *.so *.d
-root -l -q fitRecoil.C+\(\"${INPUTDIR}/WpToMuNuRecoil.root\",2,2,1,\"WmpMC\",\"W\"\)
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/WpToMuNu_S8_RecoilEvaMC.root\",2,2,1,\"WmpMC\",\"W\"\)
 rm *.so *.d
-root -l -q fitRecoil.C+\(\"${INPUTDIR}/WmToMuNuRecoil.root\",2,2,1,\"WmmMC\",\"W\"\)
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/WmToMuNu_S8_RecoilEvaMC.root\",2,2,1,\"WmmMC\",\"W\"\)
 rm *.so *.d
 
 #Electron scripts
-#INPUTDIR=/d2/scratch/knuhamid/Wpt-x-section/CMSSW_5_3_4/src/TerraNova/NtupleMaker/analysis/EventSelection/ElectronLowPU
-#INPUTDIR=/d2/scratch/khakim/WAnalysis/CMSSW_5_2_6/src/TerraNova/NtupleMaker/analysis/EventSelection/ElectronLowPU
+INPUTDIR=../WPtRecoilCorr/Electron2012LoPU
 ###                           u1 model(1:single,2:double,3:triple), u2 model, signal only
-#root -l -q fitRecoil.C+\(\"${INPUTDIR}/Ele_RD_LowPU_Recoil.root\",2,2,1,\"ZeeData\",\"Z\"\)
-#rm *.so *.d
-#root -l -q fitRecoil.C+\(\"${INPUTDIR}/Ele_DYToEE_S8_Recoil.root\",2,2,1,\"ZeeMC\",\"Z\"\)
-#rm *.so *.d
-#root -l -q fitRecoil.C+\(\"${INPUTDIR}/Ele_WpEleNu_S8_Recoil.root\",2,2,1,\"WepMC\",\"W\"\)
-#rm *.so *.d
-#root -l -q fitRecoil.C+\(\"${INPUTDIR}/Ele_WmEleNu_S8_Recoil.root\",2,2,1,\"WemMC\",\"W\"\)
-#rm *.so *.d
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/Ele_RD_LowPU_RecoilEvaRD.root\",2,2,1,\"ZeeData\",\"Z\"\)
+rm *.so *.d
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/Ele_DYToEE_S8_RecoilEvaMC.root\",2,2,1,\"ZeeMC\",\"Z\"\)
+rm *.so *.d
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/WpToEleNu_S8_RecoilEvaMC.root\",2,2,1,\"WepMC\",\"W\"\)
+rm *.so *.d
+root -l -q fitRecoil.C+\(\"${INPUTDIR}/WmToEleNu_S8_RecoilEvaMC.root\",2,2,1,\"WemMC\",\"W\"\)
+rm *.so *.d
