4f1dcd829101df2df57709fcefb49eabb4926313
jungmin <jungmin@cern.ch>
2014-06-13 21:37:28 +0900

    MVA Comparison update

--- Analysis/MVAnoPUMETana/MVA_allsrcComp.C
+++ Analysis/MVAnoPUMETana/MVA_allsrcComp.C
@@ -1,86 +1,5 @@
 #include <TLatex.h>
 
-//const int   nptBins  =   9;
-//double xbins_pt[nptBins+1] = {0.0, 7.5, 12.5, 17.5, 24,30,40,50,70,110,150,190,250,600};
-TH1D *drawDifference(TH1D *hTrue,TH1D *hist,const TString YLabel,const Double_t range,const Int_t nMarker,const Int_t nColor);
-
-TH1D *drawDifference(TH1D *hTrue,TH1D *hist, const TString YLabel,const Double_t range,const Int_t nMarker,const Int_t nColor)
-{
-  TH1D *hDiff = new TH1D(YLabel,"",20,0,100); hDiff->Sumw2();
-  Double_t diff,ratio,errDiff,errSubtr,errTrue;
-
-  TAxis *xaxis = hTrue->GetXaxis();
-  for(int ibin=1; ibin<=hTrue->GetNbinsX(); ibin++) {
-    diff = hist->GetBinContent(ibin)-hTrue->GetBinContent(ibin);
-    if (hTrue->GetBinContent(ibin)==0){
-      ratio=hist->GetBinContent(ibin);
-      errSubtr = sqrt(ratio);
-      //errDiff=sqrt(errSubtr);
-      errDiff=errSubtr;
-    }else{
-      ratio = diff/hTrue->GetBinContent(ibin);
-      errSubtr = sqrt(hTrue->GetBinError(ibin)*hTrue->GetBinError(ibin)+hist->GetBinError(ibin)*hist->GetBinError(ibin));
-      errTrue = hTrue->GetBinError(ibin);
-      errDiff=ratio*errSubtr/diff;
-      if (diff==0) errDiff = 0;
-    }
-    hDiff->SetBinContent(ibin,ratio);
-    hDiff->SetBinError(ibin,errDiff);
-  }
-  //hDiff->GetYaxis()->SetRangeUser(-2,range);
-  //hDiff->GetYaxis()->SetTitleOffset(0.6);
-  //hDiff->GetYaxis()->SetTitleSize(0.08);
-  //hDiff->GetYaxis()->SetLabelSize(0.08);
-  //hDiff->GetYaxis()->CenterTitle();
-  //hDiff->GetXaxis()->SetTitleOffset(1.2);
-  //hDiff->GetXaxis()->SetTitleSize(0.10);
-  //hDiff->GetXaxis()->SetLabelSize(0.04);
-  //hDiff->GetXaxis()->SetTitle("#slash{E}_{T} [GeV]");
-  //hDiff->GetYaxis()->SetTitle(YLabel);
-  
-  hDiff->SetMarkerStyle(nMarker);
-  hDiff->SetMarkerColor(nColor);
-  hDiff->SetLineColor(nColor);
-  hDiff->SetMarkerSize(0.9);
-
-  return hDiff;
-}
-
-void Difference(TH1* iH0,TH1 *iH1) {
-  std::string lName = std::string(iH0->GetName());
-  //TH1F *lHDiff  = new TH1F((lName+"Diff").c_str(),(lName+"Diff").c_str(),50,0,300); lHDiff->Sumw2();
-  TH1F *lHDiff  = new TH1F((lName+"Diff").c_str(),(lName+"Diff").c_str(),iH0->GetNbinsX(),iH0->GetXaxis()->GetXmin(),iH0->GetXaxis()->GetXmax()); lHDiff->Sumw2();
-  lHDiff->SetFillColor(kViolet); lHDiff->SetFillStyle(1001); lHDiff->SetLineWidth(1);
-  TH1F *lXHDiff1 = new TH1F((lName+"XDiff1").c_str(),(lName+"XDiff1").c_str(),iH0->GetNbinsX(),iH0->GetXaxis()->GetXmin(),iH0->GetXaxis()->GetXmax());
-  int i1 = 0;
-  lXHDiff1->SetLineStyle(2); lXHDiff1->SetLineWidth(2); lXHDiff1->SetLineColor(kRed);
-
-  //lHDiff->GetYaxis()->SetRangeUser(0,2);
-  //lHDiff->GetYaxis()->SetTitleOffset(0.6);
-  //lHDiff->GetYaxis()->SetTitleSize(0.08);
-  //lHDiff->GetYaxis()->SetLabelSize(0.08);
-  //lHDiff->GetYaxis()->CenterTitle();
-  //lHDiff->GetXaxis()->SetTitleOffset(1.2);
-  //lHDiff->GetXaxis()->SetTitleSize(0.10);
-  //lHDiff->GetXaxis()->SetLabelSize(0.08);
-  //lHDiff->GetYaxis()->CenterTitle();
-  //lHDiff->GetXaxis()->SetTitle("#slash{E}_{T} [GeV]");
-  //lHDiff->GetXaxis()->CenterTitle();
-  //lHDiff->GetYaxis()->SetTitle(YLabel);
-
-  for(int i0 = 0; i0 < lHDiff->GetNbinsX()+1; i0++) {
-    double lXCenter = lHDiff->GetBinCenter(i0);
-    double lXVal     = iH0   ->GetBinContent(i0);
-    lXHDiff1->SetBinContent(i0, 1.0);
-    while(iH1->GetBinCenter(i1) < lXCenter) {i1++;}
-    if(iH1->GetBinContent(i0) > 0) lHDiff->SetBinContent(i0,(lXVal-iH1->GetBinContent(i0))/(iH1->GetBinContent(i0)));
-    if(iH1->GetBinContent(i0) > 0) lHDiff->SetBinError  (i0,iH0->GetBinError(i0)/(iH1->GetBinContent(i0)));
-  }
-  lHDiff->SetMarkerStyle(kFullCircle); lHDiff->SetLineColor(kBlack); lHDiff->SetMarkerColor(kBlack);
-  lHDiff->Draw("E1");
-  lXHDiff1->Draw("hist sames");
-}
-
 void MVA_allsrcComp()
 {
   //InpFile = new TFile("../srcbatch/scTTMuon/TTsemiMuon.root");
@@ -130,23 +49,23 @@ void MVA_allsrcComp()
   TH1D *MVAmetall;
  
     sprintf(histName,"pfMet");
-    sprintf(histName_org,"pfMET_3");
+    sprintf(histName_org,"pfMET_0");
     pfMet =(TH1D*)InpFile->Get(histName_org)->Clone(histName);pfMet->Sumw2();
     
     sprintf(histName,"NoPUmet");
-    sprintf(histName_org,"NoPuMET_3");
+    sprintf(histName_org,"NoPuMET_0");
     NoPUmet =(TH1D*)InpFile->Get(histName_org)->Clone(histName);NoPUmet->Sumw2();
     
     sprintf(histName,"NoPUmetall");
-    sprintf(histName_org,"NoPuMET_3");
+    sprintf(histName_org,"NoPuMET_0");
     NoPUmetall =(TH1D*)InpFileall->Get(histName_org)->Clone(histName);NoPUmetall->Sumw2();
     
     sprintf(histName,"MVAmet");
-    sprintf(histName_org,"MVaMET_3");
+    sprintf(histName_org,"MVaMET_0");
     MVAmet =(TH1D*)InpFile->Get(histName_org)->Clone(histName);MVAmet->Sumw2();
 
     sprintf(histName,"MVAmetall");
-    sprintf(histName_org,"MVaMET_3");
+    sprintf(histName_org,"MVaMET_0");
     MVAmetall =(TH1D*)InpFileall->Get(histName_org)->Clone(histName);MVAmetall->Sumw2();
 
     pfMet->SetLineWidth(2);
@@ -211,10 +130,10 @@ void MVA_allsrcComp()
     //MVAmet->GetYaxis()->SetRangeUser(-10,35);//TThadron
     //MVAmet->GetYaxis()->SetRangeUser(0,700);//monoJet
     
-    //MVAmet->GetYaxis()->SetRangeUser(-230,200);//mSUGRA 0
-    //MVAmet->GetYaxis()->SetRangeUser(-170,240);//mSUGRA 1
-    //MVAmet->GetYaxis()->SetRangeUser(-190,190);//mSUGRA 2
-    MVAmet->GetYaxis()->SetRangeUser(-170,230);//mSUGRA 3
+    MVAmet->GetYaxis()->SetRangeUser(-90,80);//mSUGRA 0
+    //MVAmet->GetYaxis()->SetRangeUser(-130,110);//mSUGRA 1
+    //MVAmet->GetYaxis()->SetRangeUser(-80,80);//mSUGRA 2
+    //MVAmet->GetYaxis()->SetRangeUser(-140,130);//mSUGRA 3
     
     //MVAmet->GetYaxis()->SetRangeUser(-50,1000);//monoPhoton0
     //MVAmet->GetYaxis()->SetRangeUser(-50,1300);//monoPhoton2
@@ -249,23 +168,23 @@ void MVA_allsrcComp()
     line->Draw();
     
     //All 0To7 7To21 21To
-    //sprintf(histName,"muonTTJetsemiLept_PURange_21to.png");
-    //sprintf(histName,"electronTTJetsemiLept_PURange_21to.png");
-    //sprintf(histName,"monoJet_PURange_21to.png");
-    sprintf(histName,"mSUGRA_PURange_21to.png");
-    //sprintf(histName,"hadronTT2hadron_PURange_21to.png");
-    //sprintf(histName,"electronWToENu_PURange_21to.png");
-    //sprintf(histName,"muonWToMuNu_PURange_21to.png");
-    //sprintf(histName,"electronDYElEl_PURange_21to.png");
-    //sprintf(histName,"muonDYMuNu_PURange_21to.png");
-    //sprintf(histName,"ZZ4Mu_PURange_21to.png");
-    //sprintf(histName,"ZZ4El_PURange_21to.png");
-    //sprintf(histName,"ZZ2Mu2El_PURange_21to.png");
-    //sprintf(histName,"ZZ2Mu2Nu_PURange_21to.png");
-    //sprintf(histName,"ZZ2El2Nu_PURange_21to.png");
-    //sprintf(histName,"WW_PURange_21to.png");
-    //sprintf(histName,"WZ_PURange_21to.png");
-    //sprintf(histName,"monoPhoton_PURange_21to.png");
+    //sprintf(histName,"muonTTJetsemiLept_PURange_All.png");
+    //sprintf(histName,"electronTTJetsemiLept_PURange_All.png");
+    //sprintf(histName,"monoJet_PURange_All.png");
+    sprintf(histName,"mSUGRA_PURange_All.png");
+    //sprintf(histName,"hadronTT2hadron_PURange_All.png");
+    //sprintf(histName,"electronWToENu_PURange_All.png");
+    //sprintf(histName,"muonWToMuNu_PURange_All.png");
+    //sprintf(histName,"electronDYElEl_PURange_All.png");
+    //sprintf(histName,"muonDYMuNu_PURange_All.png");
+    //sprintf(histName,"ZZ4Mu_PURange_All.png");
+    //sprintf(histName,"ZZ4El_PURange_All.png");
+    //sprintf(histName,"ZZ2Mu2El_PURange_All.png");
+    //sprintf(histName,"ZZ2Mu2Nu_PURange_All.png");
+    //sprintf(histName,"ZZ2El2Nu_PURange_All.png");
+    //sprintf(histName,"WW_PURange_All.png");
+    //sprintf(histName,"WZ_PURange_All.png");
+    //sprintf(histName,"monoPhoton_PURange_All.png");
     c1.SaveAs(histName);
 
 }
